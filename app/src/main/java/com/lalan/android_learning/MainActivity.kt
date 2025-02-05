a7b9725bfe44f52377df7cb73fa53de39d73baa5
lalan-desai-28 <desailalan02@gmail.com>
2025-02-05 18:38:51 +0530

    Drawable topic task

--- app/src/main/java/com/lalan/android_learning/MainActivity.kt
+++ app/src/main/java/com/lalan/android_learning/MainActivity.kt
@@ -6,13 +6,15 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 import com.lalan.android_learning.activity.ActivityLearning
+import com.lalan.android_learning.drawable.DrawableActivity
 import com.lalan.android_learning.layout.LayoutLearning
 
 class MainActivity : AppCompatActivity() {
 
 
     private lateinit var activity_button: Button
-    private lateinit var layout_button : Button
+    private lateinit var layout_button: Button
+    private lateinit var drawable_button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,17 +22,22 @@ class MainActivity : AppCompatActivity() {
 
         activity_button = findViewById(R.id.activity_button)
         layout_button = findViewById(R.id.layout_button)
+        drawable_button = findViewById(R.id.drawable_button)
 
         activity_button.setOnClickListener {
             val intent = Intent(this, ActivityLearning::class.java)
             startActivity(intent)
         }
 
-        layout_button.setOnClickListener{
+        layout_button.setOnClickListener {
             val intent = Intent(this, LayoutLearning::class.java)
             startActivity(intent)
         }
 
+        drawable_button.setOnClickListener {
+            val intent = Intent(this, DrawableActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
