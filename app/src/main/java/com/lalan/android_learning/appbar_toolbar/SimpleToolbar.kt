bd4ba257d350b3c0e9c0e7582fbaefc7ec50d597
lalan-desai-28 <desailalan02@gmail.com>
2025-02-12 18:29:07 +0530

    Completed all the tasks

---
+++ app/src/main/java/com/lalan/android_learning/appbar_toolbar/SimpleToolbar.kt
@@ -0,0 +1,29 @@
+package com.lalan.android_learning.appbar_toolbar
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.lalan.android_learning.R
+
+class SimpleToolbar : AppCompatActivity() {
+
+    private lateinit var my_toolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simple_toolbar)
+
+        my_toolbar = findViewById(R.id.my_toolbar)
+
+        my_toolbar.setOnMenuItemClickListener {
+            Toast.makeText(this, "You clicked on the ${it.title} button.", Toast.LENGTH_SHORT)
+                .show()
+            true
+        }
+
+        my_toolbar.setNavigationOnClickListener {
+            Toast.makeText(this, "You clicked on the navigation icon.", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
