a280a317119c0ea71234517e0c04af44cde18955
lalan-desai-28 <desailalan02@gmail.com>
2025-02-25 10:09:58 +0530

    All tasks of retrofit (Get & Post)

--- app/src/main/java/com/lalan/android_learning/retrofit/services/UserService.kt
+++ app/src/main/java/com/lalan/android_learning/retrofit/services/UserService.kt
@@ -1,11 +1,20 @@
 package com.lalan.android_learning.retrofit.services
 
+import com.lalan.android_learning.retrofit.models.Credentials
+import com.lalan.android_learning.retrofit.models.LoginResponse
 import com.lalan.android_learning.retrofit.models.User
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
 
 interface UserService {
 
     @GET("/users")
-    fun getUsers() : Call<List<User>>
+    fun getUsers(): Call<List<User>>
+
+    @Headers("Content-Type: application/json")
+    @POST("/auth/login")
+    fun login(@Body credentials: Credentials): Call<LoginResponse>
 }
